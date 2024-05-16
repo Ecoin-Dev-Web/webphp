d3f31451758f1640a62fc15f7452c706804bff5d
Abdelouahab Djoudi <info@djoudi.net>
2024-05-16 09:38:30 +0100

    add cookiies

---
+++ cookies.php
@@ -0,0 +1,21 @@
+<?php
+setcookie('color','red',time()+60);
+
+$c = isset($_COOKIE['color'])?$_COOKIE['color']:null;
+
+$mylang = isset($_GET['lang'])?$_GET['lang']:'en';
+
+$multi = [
+    'ar'=> ['welcom'=>'مرحبا بكم في موقعي','link_ar'=>'العربية','link_en'=>'الإنجليزية'],
+    'en'=> ['welcom'=>'Welcom To my Web','link_ar'=>'Arabic','link_en'=>'English']
+];
+?>
+
+<h1> <?=$multi[$mylang]['welcom'] ?></h1>
+
+<li><a href="?lang=ar"><?=$multi[$mylang]['link_ar'] ?></a></li>
+<li><a href="?lang=en"><?=$multi[$mylang]['link_en'] ?></a></li>
+
+
+
+
